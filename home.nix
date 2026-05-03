02f8fb87e9fe3554fb0f6ef8e4a46badaa7064cc
luccahuguet <lucca.huguet2@gmail.com>
2026-05-02 23:51:37 -0300

    feat: add flyctl and yazelix cache

--- home.nix
+++ home.nix
@@ -28,12 +28,44 @@
         exec npx --yes vercel@53.1.0 "$@"
       '';
     };
+    flyctl = pkgs.stdenvNoCC.mkDerivation {
+      pname = "flyctl";
+      version = "0.4.45";
+
+      src = pkgs.fetchurl {
+        url = "https://github.com/superfly/flyctl/releases/download/v0.4.45/flyctl_0.4.45_Linux_x86_64.tar.gz";
+        hash = "sha256-od0QrSsWK3zQ66Xrf/n9E/3uMJpd4uwUcN30djv0xZQ=";
+      };
+
+      dontUnpack = true;
+      dontBuild = true;
+
+      installPhase = ''
+        runHook preInstall
+        tar -xzf $src flyctl
+        install -Dm755 flyctl $out/bin/flyctl
+        ln -s $out/bin/flyctl $out/bin/fly
+        runHook postInstall
+      '';
+    };
   in
 
 {
   home.username = "lucca";
   home.homeDirectory = "/home/lucca";
   home.stateVersion = "24.11";
+
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      extra-substituters = [
+        "https://yazelix.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "yazelix.cachix.org-1:ZgxIjQvaP0VTWL8Racx27mpUNzDJ97xC2y7QWYjmGNM="
+      ];
+    };
+  };
 
   home.packages =
     (with pkgs; [
@@ -51,6 +83,7 @@
       rustfmt
       clippy
       rust-analyzer
+      cachix
     ])
     ++ [
       aiPkgs.claude-code
@@ -58,6 +91,7 @@
       aiPkgs.opencode
       beadsRust
       vercelCli
+      flyctl
     ];
 
   # Apply with: home-manager switch --flake .#lucca@loqness
