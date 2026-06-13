b7fb3abcd59e311e40c739da98e7380ed2551539
luccahuguet <lucca.huguet2@gmail.com>
2026-06-13 16:42:33 -0300

    Update Yazelix runtime input

--- modules/yazelix.nix
+++ modules/yazelix.nix
@@ -4,6 +4,7 @@
     manage_config = false;
     terminal = "ratty";
     yzxterm_profile = "shaders";
+    yzxterm_emoji_font = "twitter";
     extra_terminal_launchers = [
       "ghostty"
       "yzxterm"
